25529057a58b9277195e55294550519b5f056d9f
Corey Richardson <cmr@o1labs.org>
2019-07-02 00:56:25 -0700

    lay out our and opponent's hand

--- src/main.rs
+++ src/main.rs
@@ -30,11 +30,18 @@ fn main() -> Result<(), failure::Error> {
     let mut read_buf = [0u8; 16];
     loop {
         terminal.draw(|mut f| {
+            // TODO: center CardsWidget within the list of PlaceWidget
+            let mut other_player_cards = multiway_tug::CardsWidget(&gs.players[0].hand, &gs, true);
+            let mut our_cards = multiway_tug::CardsWidget(&gs.players[1].hand, &gs, false);
+            let (ox, oy) = other_player_cards.bounds();
+            f.render(&mut other_player_cards, Rect::new(21 - (ox / 2), 0, ox, oy));
             for (place_idx, place) in gs.places.iter().enumerate() {
-                let place_rect = Rect::new(7 * (place_idx as u16), 0, 7, 21);
-                f.render(&mut multiway_tug::PlaceWidget(place), place_rect);
+                let place_rect = Rect::new(7 * (place_idx as u16), 3, 7, 21);
+                f.render(&mut multiway_tug::PlaceWidget(place, place_idx as u8), place_rect);
             }
-        })?;
+            let (ox, oy) = our_cards.bounds();
+            f.render(&mut our_cards, Rect::new(21 - (ox / 2), 14, ox, oy));
+        });
         stdin.read(&mut read_buf)?;
         match std::str::from_utf8(&read_buf[0..1])?.parse::<usize>() {
             Ok(v) =>  {
--- src/tui.rs
+++ src/tui.rs
@@ -6,7 +6,7 @@ use tui::layout::Rect;
 use tui::style::Style;
 use tui::widgets::Widget;
 
-pub fn draw_card(val: u8, count: usize) -> String {
+pub fn draw_card(val: char, count: usize) -> String {
     if count != 0 {
         format!(
             "╒═╕{topright}\n\
@@ -22,13 +22,58 @@ pub fn draw_card(val: u8, count: usize) -> String {
     }
 }
 
-pub struct PlaceWidget<'a>(pub &'a crate::mechanics::Place);
+fn char_of_digit(c: u8) -> char {
+    match c {
+        2 => '2',
+        3 => '3',
+        4 => '4',
+        5 => '5',
+        _ => panic!("shouldn't need a char for this digit: {}", c)
+    }
+}
+
+pub struct CardsWidget<'a>(pub &'a crate::mechanics::Cards, pub &'a crate::mechanics::GameState, pub bool);
+
+impl<'a> CardsWidget<'a> {
+    pub fn bounds(&self) -> (u16, u16) {
+        (3*(self.0.len() as u16), 3)
+    }
+}
+impl<'a> Widget for CardsWidget<'a> {
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        for (card_idx , card) in self.0.iter().enumerate() {
+            let color = if self.2 { tui::style::Color::Reset } else { color_from_place(card.0) };
+            let card_text = draw_card(if self.2 { ' ' } else { char_of_digit(self.1.places[card.0 as usize].value) }, 1);
+            for (count, line) in card_text
+                .lines()
+                .enumerate()
+            {
+                buf.set_string(area.left() + 3 * (card_idx as u16), area.top() + (count as u16), line, Style::default().fg(color));
+            }
+        }
+    }
+}
+pub struct PlaceWidget<'a>(pub &'a crate::mechanics::Place, pub u8);
+
+fn color_from_place(pl: u8) -> tui::style::Color {
+    use tui::style::Color::*;
+    match pl {
+        0 => Red,
+        1 => Green,
+        2 => Yellow,
+        3 => Blue,
+        4 => Magenta,
+        5 => Cyan,
+        6 => Reset,
+        _ => panic!("Too many places to color :(")
+    }
+}
 
 impl<'a> Widget for PlaceWidget<'a> {
     // A place is built around 7x7 squares on top of each other, forming a 7x21 area.
     fn draw(&mut self, area: Rect, buf: &mut Buffer) {
         // TODO: consider being more efficient.
-        let top_square = draw_card(self.0.value, self.0.scores[0] as usize);
+        let top_square = draw_card(char_of_digit(self.0.value), self.0.scores[0] as usize);
         let main_square = format!(
             "\
              ╒═════╕\n\
@@ -38,7 +83,7 @@ impl<'a> Widget for PlaceWidget<'a> {
              ╘═════╛",
             self.0.value, self.0.value
         );
-        let bottom_square = draw_card(self.0.value, self.0.scores[1] as usize);
+        let bottom_square = draw_card(char_of_digit(self.0.value), self.0.scores[1] as usize);
         for (count, line) in top_square
             .lines()
             .chain(main_square.lines())
@@ -46,7 +91,7 @@ impl<'a> Widget for PlaceWidget<'a> {
             .enumerate()
         {
             let count = count as u16;
-            buf.set_string(area.left(), area.top() + count, line, Style::default());
+            buf.set_string(area.left(), area.top() + count, line, Style::default().fg(color_from_place(self.1)));
         }
     }
 }
